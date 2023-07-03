68626204c5e44357156db4597db5ea8eae920fd5
ca-d <cad@sdf.org>
2023-07-03 11:47:20 +0000

    Deploying to gh-pages from @ ca-d/open-scd-core@1a4ad10fd57b578d154e5b129bb734490efaf3f0 🚀

--- foundation.js
+++ foundation.js
@@ -1,5 +1,4 @@
 export { newOpenEvent } from './foundation/open-event.js';
 export { handleEdit, isComplex, isInsert, isNamespaced, isRemove, isUpdate, newEditEvent } from './foundation/edit-event.js';
-export { isCreateWizard, isEditWizard, newWizardEvent } from './foundation/wizard-event.js';
 export { cyrb64 } from './foundation/cyrb64.js';
 //# sourceMappingURL=foundation.js.map
--- locales/foundation.d.ts
+++ locales/foundation.d.ts
@@ -5,6 +5,4 @@ export { newOpenEvent } from './foundation/open-event.js';
 export type { OpenEvent, OpenDetail } from './foundation/open-event.js';
 export { handleEdit, isComplex, isInsert, isNamespaced, isRemove, isUpdate, newEditEvent, } from './foundation/edit-event.js';
 export type { AttributeValue, Edit, EditEvent, Insert, NamespacedAttributeValue, Remove, Update, } from './foundation/edit-event.js';
-export { isCreateWizard, isEditWizard, newWizardEvent, } from './foundation/wizard-event.js';
-export type { CreateWizard, EditWizard, Wizard, WizardEvent, } from './foundation/wizard-event.js';
 export { cyrb64 } from './foundation/cyrb64.js';
